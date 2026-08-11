950a2574ebde904b0fad442a7d6ec01693af0085
Tuan Dzung Ngo <mail@tuandzung.me>
2026-08-11 13:55:27 +0700

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for c in s:
+            if c == '(': stack.append(')')
+            elif c == '{': stack.append('}')
+            elif c == '[': stack.append(']')
+            else:
+                if len(stack) == 0 or stack.pop() != c:
+                    return False
+        return len(stack) == 0
+        
